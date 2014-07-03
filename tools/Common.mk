1e9583fbf963fd4a5916b4c5ef3c0d7c394f93f6
Richard Bruce <rbruce@eclipseoptions.com>
2014-07-03 13:46:48 +0800

    updated to new makefile system

--- tools/Common.mk
+++ tools/Common.mk
@@ -7,18 +7,30 @@ DEBUG_OPT   = $(COMMON_OPT) -g3 -O0 --coverage
 RELEASE_OPT = $(COMMON_OPT) -O3 -mfpmath=sse -msse4a -fexpensive-optimizations #-march=native
 ifeq ($(BUILD), DEBUG)
 	LD_OPT = --coverage
-	CC_OPT = $(DEBUG_OPT)
+	CC_OPT = $(DEBUG_OPT) --std=c11
+    CPP_OPT = $(DEBUG_OPT) --std=c++1y
 else
 	LD_OPT =
-	CC_OPT = $(RELEASE_OPT)
+	CC_OPT = $(RELEASE_OPT) --std=c11
+    CPP_OPT = $(RELEASE_OPT) --std=c++1y
 endif
 
 # Derived files
-OBJECTS = $(SOURCE:%.cc=build/%.o)
+C_SOURCE = $(filter-out %.cc, $(SOURCE))
+CPP_SOURCE = $(filter-out %.c, $(SOURCE))
+
+C_OBJECTS = $(C_SOURCE:%.c=build/%.o)
+CPP_OBJECTS = $(CPP_SOURCE:%.cc=build/%.o)
 
 # Compile source code to object files
-build/%.o : %.cc
-	$(CPP) $(CC_OPT) $(patsubst %,-D%,$(DEFINES)) $(patsubst %,-I%,$(INCLUDE)) -c $< -o $@
+build :
+	mkdir build
+
+build/%.o : %.cc build
+	$(CPP) $(CPP_OPT) $(patsubst %,-D%,$(DEFINES)) $(patsubst %,-I%,$(INCLUDE)) -c $< -o $@
+
+build/%.o : %.c build
+	$(CC) $(CC_OPT) $(patsubst %,-D%,$(DEFINES)) $(patsubst %,-I%,$(INCLUDE)) -c $< -o $@
 
 # Lint code
 cppcheck.xml ::
