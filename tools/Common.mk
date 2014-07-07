382821106d1611d9b30cc5bb9ca2366e6e5129e5
Richard Bruce <rbruce@eclipseoptions.com>
2014-07-07 08:26:04 +0800

    continued overhaul of makefile system

--- tools/Common.mk
+++ tools/Common.mk
@@ -2,17 +2,17 @@ CPP = g++
 CC = gcc
 
 # Compile and linker options 
-COMMON_OPT  = -std=c++1y -fPIC -Wno-unused-local-typedefs -Wall -pipe
+COMMON_OPT  = -fPIC -Wno-unused-local-typedefs -Wall -pipe
 DEBUG_OPT   = $(COMMON_OPT) -g3 -O0 --coverage
 RELEASE_OPT = $(COMMON_OPT) -O3 -mfpmath=sse -msse4a -fexpensive-optimizations #-march=native
 ifeq ($(BUILD), DEBUG)
 	LD_OPT = --coverage
 	CC_OPT = $(DEBUG_OPT) --std=c11
-    CPP_OPT = $(DEBUG_OPT) --std=c++1y
+	CPP_OPT = $(DEBUG_OPT) --std=c++1y
 else
 	LD_OPT =
 	CC_OPT = $(RELEASE_OPT) --std=c11
-    CPP_OPT = $(RELEASE_OPT) --std=c++1y
+	CPP_OPT = $(RELEASE_OPT) --std=c++1y
 endif
 
 # Derived files
@@ -26,10 +26,10 @@ CPP_OBJECTS = $(CPP_SOURCE:%.cc=build/%.o)
 build :
 	@mkdir build
 
-build/%.o : %.cc build
+build/%.o : %.cc | build
 	$(CPP) $(CPP_OPT) $(patsubst %,-D%,$(DEFINES)) $(patsubst %,-I%,$(INCLUDE)) -c $< -o $@
 
-build/%.o : %.c build
+build/%.o : %.c | build
 	$(CC) $(CC_OPT) $(patsubst %,-D%,$(DEFINES)) $(patsubst %,-I%,$(INCLUDE)) -c $< -o $@
 
 # Link objects to libraries
@@ -42,5 +42,5 @@ build/%.o : %.c build
 	ar rcs $@ $(CPP_OBJECTS) $(C_OBJECTS)
 
 # Lint code
-cppcheck.xml ::
+cppcheck.xml : $(SOURCE)
 	cppcheck  $(patsubst %,-I%,$(LOCAL_INCLUDES)) --enable=all --error-exitcode=1 --force --inline-suppr --suppress=missingIncludeSystem --xml-version=2 ./ 2> cppcheck.xml
--- tools/UnitTest.mk
+++ tools/UnitTest.mk
@@ -5,11 +5,12 @@ endif
 include $(RAPTOR_TOOLS)/Common.mk
 
 # Derived files
-TEST_OBJECTS = $(SOURCE:%.cc=build/%.o)
-EXES = $(SOURCE:.cc=.out)
+TEST_OBJECTS = $(filter-out %.c, $(filter-out %.cc, $(SOURCE:%.cc=build/%.o) $(SOURCE:%.c=build/%.o)))
+EXES = $(filter-out %.c, $(SOURCE:.cc=.out))
 
 # Includes
 INCLUDE += $(LIBARYS_PATH)/teamcity ../
+vpath %.c $(INCLUDE)
 vpath %.cc $(INCLUDE)
 
 # Libraries
@@ -24,17 +25,17 @@ main.out : $(TEST_OBJECTS)
 
 # Build individual test suites
 define test_suite_template
-$(1) : $(patsubst %, build/%, $(2)) $(patsubst %.out, %.cc, $(1))
-	$(CPP) $(CC_OPT) -DSTAND_ALONE $(patsubst %,-D%,$(DEFINES)) $(patsubst %,-I%,$(INCLUDE)) -c $(patsubst %.out, %.cc, $(1)) -o $(patsubst %.out, build/%.o, $(1))
-	$(CPP) $(LD_OPT) $(patsubst %, build/%, $(2)) $(patsubst %.out, build/%.o, $(1)) $(patsubst %,-L%,$(LIBPATH)) $(patsubst %,-l%,$(LIBRARY)) -o $(1) 
+$(1) : $(patsubst %, build/%, $(filter-out $(patsubst %.out, %.o, $(1)), $(2))) $(patsubst %.out, %.cc, $(1))
+	$(CPP) $(CPP_OPT) -DSTAND_ALONE $(patsubst %,-D%,$(DEFINES)) $(patsubst %,-I%,$(INCLUDE)) -c $(patsubst %.out, %.cc, $(1)) -o $(patsubst %.out, build/%.o, $(1))
+	$(CPP) $(LD_OPT) $(patsubst %, build/%, $(filter-out $(patsubst %.out, %.o, $(1)), $(2))) $(patsubst %.out, build/%.o, $(1)) $(patsubst %,-L%,$(LIBPATH)) $(patsubst %,-l%,$(LIBRARY)) -o $(1) 
 endef
 
 # Clean targets
 clean ::
-	$(RM) build/*
+	$(RM) -r build
 	$(RM) $(EXES)
 
 spotless ::
-	$(RM) -rf test_coverage
-	$(RM) build/*
+	$(RM) -r test_coverage
+	$(RM) -r build
 	$(RM) $(EXES)
