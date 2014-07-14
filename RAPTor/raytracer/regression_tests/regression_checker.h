f319006722b126c44b0bd9a104c443e0bd21df56
Richard Bruce <rbruce@eclipseoptions.com>
2014-07-14 10:17:32 +0800

    added checker cut off

--- RAPTor/raytracer/regression_tests/regression_checker.h
+++ RAPTor/raytracer/regression_tests/regression_checker.h
@@ -20,6 +20,7 @@
 using namespace raptor_raytracer;
 
 const float result_tolerance = 0.0005;
+const int failure_limit = 1000;
 const std::string test_data_location = "test_data/";
 
 
@@ -57,22 +58,33 @@ class regression_checker : private boost::noncopyable
             unsigned int exp_y_res;
             std::unique_ptr<unsigned char []> expected(new unsigned char [image_size]);
             read_png_file(_expected, expected.get(), &exp_x_res, &exp_y_res);
-            BOOST_CHECK(x_res == exp_x_res);
-            BOOST_CHECK(y_res == exp_y_res);
+
+            /* Check image dimensions, if this fails theres no point even trying */
+            BOOST_ASSERT(x_res == exp_x_res);
+            BOOST_ASSERT(y_res == exp_y_res);
 
             /* Compare */
+            int failures = 0;
             std::unique_ptr<unsigned char []> difference(new unsigned char [image_size]);
             for (unsigned int i = 0; i < image_pixels; ++i)
             {
                 /* Check */
                 const unsigned int idx = i * 3;
-                BOOST_CHECK(actual[idx    ] == expected[idx    ]);
-                BOOST_CHECK(actual[idx + 1] == expected[idx + 1]);
-                BOOST_CHECK(actual[idx + 2] == expected[idx + 2]);
+                if (failures < failure_limit)
+                {
+                    BOOST_CHECK(actual[idx    ] == expected[idx    ]);
+                    BOOST_CHECK(actual[idx + 1] == expected[idx + 1]);
+                    BOOST_CHECK(actual[idx + 2] == expected[idx + 2]);
+                }
+                else if (failures == failure_limit)
+                {
+                    BOOST_LOG_TRIVIAL(warning) << failure_limit << " errors detected, I'm not checking anymore. Go fix your code";
+                }
 
                 /* Colour mismatch pixels red */
                 if ((actual[idx] != expected[idx]) || (actual[idx + 1] != expected[idx + 1]) || (actual[idx + 2] != expected[idx + 2]))
                 {
+                    ++failures;
                     difference[idx    ] = 255;
                     difference[idx + 1] = 0;
                     difference[idx + 2] = 0;
