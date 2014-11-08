b43c656ab2a04e8619758b112e5886ad47fa47c6
RichardBruce <richard.bruce84@gmail.com>
2014-11-08 20:32:45 +0800

    fixed unused function link

--- RAPTor/raytracer/regression_tests/cfg_tests.cc
+++ RAPTor/raytracer/regression_tests/cfg_tests.cc
@@ -12,7 +12,7 @@ const raptor_physics::init_logger init_logger;
 #include "regression_fixture.h"
 
 
-BOOST_AUTO_TEST_SUITE( cfg_tests )
+BOOST_AUTO_TEST_SUITE( cfg_tests );
 
 BOOST_AUTO_TEST_CASE( cfg_mgftree_test )
 {
