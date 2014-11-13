31c777e9343a9ea51d8a93388b82e9495f6259a2
RichardBruce <richard.bruce84@gmail.com>
2014-11-13 18:59:28 +0800

    moved from fp_t to flow and a little other clean up

--- RAPTor/raytracer/materials/cook_torrance_cxy.h
+++ RAPTor/raytracer/materials/cook_torrance_cxy.h
@@ -15,44 +15,44 @@ class cook_torrance_cxy : public material
 {
     public :
         /* Constructor for non light emmitting object */
-        cook_torrance_cxy(const fp_t x=0.33, const fp_t y=0.33, const fp_t rd=0.5, const fp_t td=0.0, const fp_t rs=0.0, const fp_t ts=0.0, const fp_t sr=0.0, const fp_t tr=0.0, const fp_t ri_r=1.0, const fp_t ri_i=0.0) : 
-            material(ts > (fp_t)0.0), x(x), y(y), rd(rd), td(td), rs(rs), ts(ts), sr(sr), tr(tr), ri_r(ri_r), ri_i(ri_i)
+        cook_torrance_cxy(const float x = 0.33f, const float y = 0.33f, const float rd = 0.5f, const float td = 0.0f, const float rs = 0.0f, const float ts = 0.0f, const float sr = 0.0f, const float tr = 0.0f, const float ri_r = 1.0f, const float ri_i = 0.0f) : 
+            material(ts > 0.0f), x(x), y(y), rd(rd), td(td), rs(rs), ts(ts), sr(sr), tr(tr), ri_r(ri_r), ri_i(ri_i)
             {
                 /* A whole bunch of sanity checking asserts */
                 /* Note x, y and rd are stored in light_intensity in the format ( x, rd, y ) */
-                assert((      x    >= (fp_t)0.0) && (      x    <= (fp_t)1.0));
-                assert((      y    >= (fp_t)0.0) && (      y    <= (fp_t)1.0));
-                assert((      rd   >= (fp_t)0.0) && (      rd   <= (fp_t)1.0));
-                assert((this->td   >= (fp_t)0.0) && (this->td   <= (fp_t)1.0));
-                assert((this->rs   >= (fp_t)0.0) && (this->rs   <= (fp_t)1.0));
-                assert((this->ts   >= (fp_t)0.0) && (this->ts   <= (fp_t)1.0));
-                assert((this->sr   >= (fp_t)0.0) && (this->sr   <= (fp_t)0.5));   /* In reality numbers greater than 0.2 are rare */
-                assert((this->tr   >= (fp_t)0.0) && (this->tr   <= (fp_t)1.0));
-                assert((this->ri_r >= (fp_t)0.0) && (this->ri_r <= (fp_t)5.0));
-                assert((this->ri_i >= (fp_t)0.0) && (this->ri_i <= (fp_t)1.0));
+                assert((      x    >= 0.0f) && (      x    <= 1.0f));
+                assert((      y    >= 0.0f) && (      y    <= 1.0f));
+                assert((      rd   >= 0.0f) && (      rd   <= 1.0f));
+                assert((this->td   >= 0.0f) && (this->td   <= 1.0f));
+                assert((this->rs   >= 0.0f) && (this->rs   <= 1.0f));
+                assert((this->ts   >= 0.0f) && (this->ts   <= 1.0f));
+                assert((this->sr   >= 0.0f) && (this->sr   <= 0.5f));   /* In reality numbers greater than 0.2 are rare */
+                assert((this->tr   >= 0.0f) && (this->tr   <= 1.0f));
+                assert((this->ri_r >= 0.0f) && (this->ri_r <= 5.0f));
+                assert((this->ri_i >= 0.0f) && (this->ri_i <= 1.0f));
             };
         virtual ~cook_torrance_cxy() { };
 
         /* Function the allow the shader a chance to generate SIMD packets */
-        void generate_rays(const ray_trace_engine &r, ray &i, const line &n, const hit_t h, ray *const rl, ray *const rf, fp_t *const n_rl, fp_t *const n_rf) const;
+        void generate_rays(const ray_trace_engine &r, ray &i, const line &n, const hit_t h, ray *const rl, ray *const rf, float *const n_rl, float *const n_rf) const;
 
         /* Shading function. To allow the shader to shade the current object */
         void shade(const ray_trace_engine &r, ray &i, const line &n, const hit_t h, ext_colour_t *const c, const point_t &vt) const;
 
         /* Function to the allow the shader a combined SIMD packets traced secondary rays into the image */
-        void combind_secondary_rays(const ray_trace_engine &r, ext_colour_t &c, const ray *const rl, const ray *const rf, const ext_colour_t *const c_rl, const ext_colour_t *const c_rf, const fp_t *const n_rl, const fp_t *const n_rf) const;
+        void combind_secondary_rays(const ray_trace_engine &r, ext_colour_t &c, const ray *const rl, const ray *const rf, const ext_colour_t *const c_rl, const ext_colour_t *const c_rf, const float *const n_rl, const float *const n_rf) const;
 
     private :
-        const fp_t  x;      /* X chroma                                 */
-        const fp_t  y;      /* Y chroma                                 */
-        const fp_t  rd;     /* Diffuse reflectance                      */
-        const fp_t  td;     /* Diffuse transmittance                    */
-        const fp_t  rs;     /* Specular reflectance                     */
-        const fp_t  ts;     /* Specular transmittance                   */
-        const fp_t  sr;     /* Surface roughness                        */
-        const fp_t  tr;     /* Effective transmitted surface roughness  */
-        const fp_t  ri_r;   /* Real refractive index                    */
-        const fp_t  ri_i;   /* Imaginery refractive index               */
+        const float x;      /* X chroma                                 */
+        const float y;      /* Y chroma                                 */
+        const float rd;     /* Diffuse reflectance                      */
+        const float td;     /* Diffuse transmittance                    */
+        const float rs;     /* Specular reflectance                     */
+        const float ts;     /* Specular transmittance                   */
+        const float sr;     /* Surface roughness                        */
+        const float tr;     /* Effective transmitted surface roughness  */
+        const float ri_r;   /* Real refractive index                    */
+        const float ri_i;   /* Imaginery refractive index               */
 };
 }; /* namespace raptor_raytracer */
 
