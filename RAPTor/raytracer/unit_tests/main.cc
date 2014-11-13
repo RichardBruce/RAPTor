f15ab83fec5e7a69e3aa1fdc593cc9dcaddb32b9
RichardBruce <richard.bruce84@gmail.com>
2014-11-13 19:44:42 +0800

    added main for top level tests

---
+++ RAPTor/raytracer/unit_tests/main.cc
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE main test
+
+/* Boost headers */
+#include "boost/test/unit_test.hpp"
+#include "boost/test/unit_test_suite_impl.hpp"
+
+/* Common headers */
+#include "logging.h"
+
+/* Initialise logger */
+const raptor_physics::init_logger init_logger;
