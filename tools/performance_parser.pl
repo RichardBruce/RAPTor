55e201a67d3c8e823e199af12f21b2ea7500435f
RichardBruce <richard.bruce84@gmail.com>
2014-11-27 19:27:07 +0800

    no able to handle unit and regression tests, ignore missing file and dump csv

--- tools/performance_parser.pl
+++ tools/performance_parser.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 use strict;
+use Getopt::Long;
 use Cwd;
 
 use DateTime::Format::Strptime;
@@ -9,11 +10,45 @@ my $parser = DateTime::Format::Strptime->new(
     on_error => 'croak',
 );
 
-# Check usage
-($#ARGV != 2)  or die "Two arguments expect " . $#ARGV . " given\n";
+# Get inputs
+my $log_file;
+my $test_dir;
+my $website_dir;
+GetOptions( "website_dir=s" => \$website_dir,
+            "test_dirs=s"   => \$test_dir,
+            "log_file=s"    => \$log_file) or die("Error: Cant parse command line arguments\n");
 
+###############################################
+# Check there is atleast 1 output file
+###############################################
+my @log_files;
+my @test_dirs = split(',', $test_dir);
 
+for (@test_dirs)
+{
+    # Check the file exists
+    my $log_path = "$_/$log_file";
+    if (-e $log_path)
+    {
+        # Check the file isnt empty
+        open (CHECK_DATA, "<", $log_path) or die "Error: Can't find project index.html ($log_path)";
+        my $line = <CHECK_DATA>;
+        if ($line ne "")
+        {
+            push @log_files, $log_path;
+        }
+    }
+}
+
+if ($#log_files < 0)
+{
+    print "No log files found\n";
+    exit 0;
+}
+
+###############################################
 # Create output dir if needed
+###############################################
 my $dir = cwd;
 my $project_name;
 if ($dir =~ m/(\w+)$/)
@@ -28,13 +63,14 @@ else
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
 my $date        =  ($year + 1900) . "-" . ($mon + 1) . "-" . $mday;
 my $today_date  = $parser->parse_datetime($date);
-my $output_dir  = $ARGV[1] . $project_name . "/" .  $date;
+my $output_dir  = $website_dir . $project_name . "/" .  $date;
 `mkdir -p $output_dir`;
 
-
+###############################################
 # Amend the index with a link to todays data
-my $project_index = $ARGV[1] . $project_name . "/index.html";
-my $new_project_index = $ARGV[1] . $project_name . "/index1.html";
+###############################################
+my $project_index = $website_dir . $project_name . "/index.html";
+my $new_project_index = $website_dir . $project_name . "/index1.html";
 open (INDEX_T, "<", $project_index) or die "Error: Can't find project index.html ($project_index)";
 open (INDEX_F, ">", $new_project_index) or die "Error: Can't open new project index.html";
 while (my $line = <INDEX_T>)
@@ -58,51 +94,55 @@ while (my $line = <INDEX_T>)
         $line = <INDEX_T>;      # <tr>
         print INDEX_F $line;
 
-        # Check if todays date was already add and if not add it
+        # Check if todays date was already added, if so skip it
         $line = <INDEX_T>;
-        if ($line !~ m/$date/)
+        if ($line =~ m/$date/)
         {
-            # Parse date
-            my $days_since_run;
-            if ($line =~ m/(\d{4}-\d{2}-\d{2})/)
-            {
-                my $last_date = $parser->parse_datetime($1);
-                my $dur = $today_date->delta_days($last_date);
-                $days_since_run = $dur->delta_days;
-            }
-            else
-            {
-                die "Error: Cant find date in index: $line\n";
-            }
-
-            # Get git log
-            my @git_log = `git log --since=${days_since_run}.day --pretty=oneline`;
-
-            # Pull out the latest hash
-            my $git_hash;
-            if ($git_log[0] =~ m/(\w+)\s/)
-            {
-                $git_hash = $1;
-            }
-            else
-            {
-                die "Error: Cant find hash in git log\n";
-            }
-
-            # Filter the performance related messages
-            my @perf_messages;
-            foreach (@git_log)
-            {
-                if (m/PERF:\s+([\w\s\.]+)$/)
-                {
-                    push @perf_messages, $1;
-                }
-            }
-
-            print INDEX_F "            <td><a href=\"./$date\">$date</a></td><td>$git_hash</td><td>" . join(', ', @perf_messages) , "</td>\n";
-            print INDEX_F "        </tr>\n";
-            print INDEX_F "        <tr>\n";
+            $line = <INDEX_T>;      # </tr>
+            $line = <INDEX_T>;      # <tr>
+            $line = <INDEX_T>;
         }
+
+        # Parse date
+        my $days_since_run;
+        if ($line =~ m/(\d{4}-\d{2}-\d{2})/)
+        {
+            my $last_date = $parser->parse_datetime($1);
+            my $dur = $today_date->delta_days($last_date);
+            $days_since_run = $dur->delta_days;
+        }
+        else
+        {
+            die "Error: Cant find date in index: $line\n";
+        }
+
+        # Get git log
+        my @git_log = `git log --since=${days_since_run}.day --pretty=oneline`;
+
+        # Pull out the latest hash
+        my $git_hash;
+        if ($git_log[0] =~ m/(\w+)\s/)
+        {
+            $git_hash = $1;
+        }
+        else
+        {
+            die "Error: Cant find hash in git log\n";
+        }
+
+        # Filter the performance related messages
+        my @perf_messages;
+        foreach (@git_log)
+        {
+            if (m/PERF:\s+([\w\s\.]+)$/)
+            {
+                push @perf_messages, $1;
+            }
+        }
+
+        print INDEX_F "            <td><a href=\"./$date\">$date</a></td><td>$git_hash</td><td>" . join(', ', @perf_messages) , "</td>\n";
+        print INDEX_F "        </tr>\n";
+        print INDEX_F "        <tr>\n";
     }
 
     print INDEX_F $line;
@@ -112,8 +152,9 @@ close (INDEX_F);
 
 `mv  $new_project_index $project_index`;
 
-
-# Open output file
+###############################################
+# Write new output file
+###############################################
 open (OUTFILE, ">", $output_dir . "/index.html") or die "Error: Can't open output file: $output_dir";
 
 # Output html header
@@ -131,77 +172,90 @@ print OUTFILE "    padding: 5px;\n";
 print OUTFILE "}\n";
 print OUTFILE "</style>\n";
 print OUTFILE "</head>\n";
-
 print OUTFILE "<body>\n";
-print OUTFILE "<table style=\"width:100%\">\n";
-print OUTFILE "    <tr>\n";
-print OUTFILE "        <th>Scene</th>\n";
-print OUTFILE "        <th># Primitives</th>\n";
-print OUTFILE "        <th># Lights</th>\n";
-print OUTFILE "        <th>Parser Time ms</th>\n";
-print OUTFILE "        <th>Render Time ms</th>\n";
-print OUTFILE "    </tr>\n";
-
-my $scene       = "No Scenes Found";
-my $primitives  = "--";
-my $lights      = "--";
-my $parser_time = "--";
-my $render_time = "--";
+print OUTFILE "<h1>Performance as of $date</h1>\n";
 
 # Open input file and parse
-open (LOGFILE, "<", $ARGV[0]) or die "Error: Can't find input file";
-while (<LOGFILE>)
+my %headers;
+my ( $scene, $primitives, $lights, $parser_time, $render_time );
+for (@log_files)
 {
-    chomp;
-    if (m/PERF - Scene:\s+.*\/(\w+\/\w+\/\w+\.\w+)/)
+    open (LOGFILE, "<", $_) or die "Error: Can't find input file";
+
+    $_ =~ m/(\w+)/;
+
+    open (CSVFILE, ">", $output_dir . "/$1.csv") or die "Error: Can't open output file: $output_dir/$1.csv";
+    print OUTFILE "<h2>$1</h2>\n";
+    print OUTFILE "<table style=\"width:100%\">\n";
+    print OUTFILE "    <tr>\n";
+    print OUTFILE "        <th>Scene</th>\n";
+    print OUTFILE "        <th># Primitives</th>\n";
+    print OUTFILE "        <th># Lights</th>\n";
+    print OUTFILE "        <th>Parser Time ms</th>\n";
+    print OUTFILE "        <th>Render Time ms</th>\n";
+    print OUTFILE "    </tr>\n";
+    
+    print CSVFILE "Scene,# Primitives,# Lights,Parser Time ms,Render Time ms\n";
+
+    $scene       = "No Scenes Found";
+    $primitives  = "--";
+    $lights      = "--";
+    $parser_time = "--";
+    $render_time = "--";
+    while (<LOGFILE>)
     {
-        # Output last scene
-        if ($scene ne "No Scenes Found")
+        chomp;
+        if (m/PERF - Scene:\s+.*\/(\w+\/\w+\/\w+\.\w+)/)
         {
-            output_scene();
+            # Output last scene
+            if ($scene ne "No Scenes Found")
+            {
+                output_scene();
+            }
+
+            # Graph new scene name
+            $scene = $1;
+
+            # Reset data
+            $primitives     = "--";
+            $lights         = "--";
+            $parser_time    = "--";
+            $render_time    = "--";
         }
+        elsif (m/PERF - # Primitives:\s+(\d+)/)
+        {
+            # Get primitive counts
+            $primitives = $1;
+        }
+        elsif (m/PERF - # Lights:\s+(\d+)/)
+        {
+            # Get light counts
+            $lights     = $1;
+        }
+        elsif (m/PERF - Parser Time ms:\s+(\d+)/)
+        {
+            # Get parser run time
+            $parser_time = $1;
+        }
+        elsif (m/PERF - Render Time ms:\s+(\d+)/)
+        {
+            # Get rendering run time
+            $render_time = $1;
+        }
+    }
 
-        # Graph new scene name
-        $scene = $1;
+    # Output the last scene
+    output_scene();
 
-        # Reset data
-        $primitives     = "--";
-        $lights         = "--";
-        $parser_time    = "--";
-        $render_time    = "--";
-    }
-    elsif (m/PERF - # Primitives:\s+(\d+)/)
-    {
-        # Get primitive counts
-        $primitives = $1;
-    }
-    elsif (m/PERF - # Lights:\s+(\d+)/)
-    {
-        # Get light counts
-        $lights     = $1;
-    }
-    elsif (m/PERF - Parser Time ms:\s+(\d+)/)
-    {
-        # Get parser run time
-        $parser_time = $1;
-    }
-    elsif (m/PERF - Render Time ms:\s+(\d+)/)
-    {
-        # Get rendering run time
-        $render_time = $1;
-    }
+    print OUTFILE "</table>\n";
 }
 
-# Output the last scene
-output_scene();
-
 # Output html footer
-print OUTFILE "</table>\n";
-
 print OUTFILE "</body>\n";
 print OUTFILE "</html>\n";
 
 # Clean up
+close (CSVFILE);
 close (LOGFILE);
 close (OUTFILE);
 exit 0;
@@ -216,4 +270,6 @@ sub output_scene
     print OUTFILE "        <td>${parser_time}</td>\n";
     print OUTFILE "        <td>${render_time}</td>\n";
     print OUTFILE "    </tr>\n";
+
+    print CSVFILE "${scene},${primitives},${lights},${parser_time},${render_time}\n";
 }
