5f471cee84fe8ace90ee2e98878235c3aa07c206
Richard Bruce <richard.bruce84@gmail.com>
2014-11-29 18:49:04 +0800

    fixed cant mv when no bugs found

--- tools/run_scan_build.sh
+++ tools/run_scan_build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 MOVE_TO="$1/${PWD##*/}/"
-make clean && scan-build -o ./static_analysis make all && mv static_analysis/*/* $MOVE_TO && rm -rf static_analysis/
+
+# Clean destination
+rm -rf $MOVE_TO/*
+
+# Run scan-build
+make clean && scan-build -o ./static_analysis make all
+
+# Move over the results if any
+[ "$(ls -A static_analysis)" ] && mv static_analysis/*/* $MOVE_TO || echo "No bugs"
+rm -rf static_analysis/
