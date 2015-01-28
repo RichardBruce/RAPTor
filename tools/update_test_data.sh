16d0321f0b538162c85cd87ae2aa89ce79a34bd9
RichardBruce <richard.bruce84@gmail.com>
2015-01-28 18:56:38 +0800

    Allowed selection of different test files to become the expected, but defaulting to the previour behaviour

--- tools/update_test_data.sh
+++ tools/update_test_data.sh
@@ -1,1 +1,8 @@
-for file in $1/*act* ; do mv $file ${file/act/exp}; done
+FROM_FILE="act"
+
+# Pick golden files
+if [ ! -z "$2" ]; then
+    FROM_FILE="$2"
+fi
+
+for file in $1/*$FROM_FILE* ; do mv $file ${file/$FROM_FILE/exp}; done
