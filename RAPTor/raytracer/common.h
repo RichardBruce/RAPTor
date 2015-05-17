21ce427524f6e7f2726ab0897896d81620341183
RichardBruce <richard.bruce84@gmail.com>
2015-05-17 22:03:33 +0800

    Increased max kdt node size to 30 (from 20)

--- RAPTor/raytracer/common.h
+++ RAPTor/raytracer/common.h
@@ -106,7 +106,7 @@ namespace raptor_raytracer
 
 #ifdef SIMD_PACKET_TRACING
 #ifndef MAX_KDT_NODE_SIZE
-#define MAX_KDT_NODE_SIZE 20
+#define MAX_KDT_NODE_SIZE 30
 #endif /* #ifndef MAX_KDT_NODE_SIZE */
 #endif /* #ifdef SIMD_PACKET_TRACING */
 
