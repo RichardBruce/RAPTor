7be04ab1ad31543b2c456ed6cdce28618b7957ae
RichardBruce <richard.bruce84@gmail.com>
2015-05-20 22:00:37 +0800

    kdt node size to 35

--- RAPTor/raytracer/common.h
+++ RAPTor/raytracer/common.h
@@ -106,7 +106,7 @@ namespace raptor_raytracer
 
 #ifdef SIMD_PACKET_TRACING
 #ifndef MAX_KDT_NODE_SIZE
-#define MAX_KDT_NODE_SIZE 5
+#define MAX_KDT_NODE_SIZE 35
 #endif /* #ifndef MAX_KDT_NODE_SIZE */
 #endif /* #ifdef SIMD_PACKET_TRACING */
 
