73357b5b6f7742c66d8cb493223f467d29d538a7
RichardBruce <richard.bruce84@gmail.com>
2016-01-25 22:31:44 +0800

    Added logging

--- RAPTor/convex_decomposition/Project.mk
+++ RAPTor/convex_decomposition/Project.mk
@@ -5,14 +5,14 @@ SOURCE  = main.cc convex_decomposition.cc dac_convex_hull.cc voxel_set.cc tetrah
 INCLUDE = $(LOCAL_INCLUDES) \
     ${BOOST_INCLUDE_PATH} \
     $(LIBARYS_PATH)/tbb$(TBB_VER)/include/tbb/ 
-LOCAL_INCLUDES  = . $(RAPTOR_HOME)/common
+LOCAL_INCLUDES  = . $(RAPTOR_HOME)/common $(RAPTOR_HOME)/parsers
 vpath %.cc $(LOCAL_INCLUDES)
 
 # Libraries
 LIBPATH = $(LIBARYS_PATH)/tbb$(TBB_VER)/build/build_release \
     ${BOOST_LIB_PATH}
-SO_LIBS = tbb boost_thread boost_filesystem boost_system boost_log boost_serialization
+SO_LIBS = tbb pthread boost_system boost_filesystem boost_log boost_serialization boost_program_options
 LIBRARY = $(SO_LIBS)
 
 # Defines
-DEFINES = BOOST_LOG_LEVEL=boost::log::trivial::trace
+DEFINES = BOOST_LOG_DYN_LINK BOOST_LOG_LEVEL=boost::log::trivial::trace
