09d793d43aa58ab1aa389302e373258ed5dd8f0a
RichardBruce <richard.bruce84@gmail.com>
2016-01-31 11:29:37 +0800

    Upgrade scan-build version

--- tools/run_scan_build.sh
+++ tools/run_scan_build.sh
@@ -6,7 +6,7 @@ MOVE_TO="$1/$GIT_BRANCH/$2/${PWD##*/}/"
 rm -rf $MOVE_TO/*
 
 # Run scan-build
-make clean && scan-build -o ./static_analysis make all
+make clean && scan-build-3.5 -o ./static_analysis make all
 
 # Move over the results if any
 [ "$(ls -A static_analysis)" ] && mv static_analysis/*/* $MOVE_TO || echo "No bugs"
